0b2dd9bdc6e259e5a04311e6ae200189e405a6e1
Antonio Bertuccio <anto.bertu@gmail.com>
2023-10-12 18:09:47 +0200

    add extra

--- javascript/script.js
+++ javascript/script.js
@@ -41,3 +41,49 @@ extractNewNumber.addEventListener("click", () => {
 });
 
 newGameButton.addEventListener("click", resetGame);
+
+document
+  .getElementById("generate-player-cards")
+  .addEventListener("click", function () {
+    const numberOfCards = parseInt(
+      document.getElementById("number-of-cards").value
+    );
+    const container = document.querySelector(".player-cards");
+
+    function generateRandomNumbers(count) {
+      const numbers = [];
+      while (numbers.length < count) {
+        const randomNum = Math.floor(Math.random() * 90) + 1;
+        if (!numbers.includes(randomNum)) {
+          numbers.push(randomNum);
+        }
+      }
+      return numbers;
+    }
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < numberOfCards; i++) {
+      const playerCard = document.createElement("div");
+      playerCard.className = "player-card";
+
+      const table = document.createElement("table");
+      const tbody = document.createElement("tbody");
+
+      for (let row = 0; row < 3; row++) {
+        const tr = document.createElement("tr");
+        for (let col = 0; col < 5; col++) {
+          const td = document.createElement("td");
+          const numbers = generateRandomNumbers(15);
+          td.textContent = numbers[col + row * 5];
+          td.classList.add("card-number-" + numbers[col + row * 5]);
+          tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+      }
+
+      table.appendChild(tbody);
+      playerCard.appendChild(table);
+      container.appendChild(playerCard);
+    }
+  });
